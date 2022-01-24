4d5bd3bdcb0a0d64ddabcc57ec209bf981e43a7e
VenkataSudhakar(VESU) Rayaprolu <vesu.rayaprolu@kellogg.northwestern.edu>
2022-01-24 02:23:25 +0000

    Old version - Needs fix

---
+++ kmdb_old.sql
@@ -0,0 +1,148 @@
+-- In this assignment, you'll be building the domain model, database 
+-- structure, and data for "KMDB" (the Kellogg Movie Database).
+-- The end product will be a report that prints the movies and the 
+-- top-billed cast for each movie in the database.
+
+-- Requirements/assumptions
+--
+-- - There will only be three movies in the database – the three films
+--   that make up Christopher Nolan's Batman trilogy
+-- - Movie data includes the movie title, year released, MPAA rating,
+--   and director
+-- - A movie has a single director
+-- - A person can be the director of and/or play a role in a movie
+-- - Everything you need to do in this assignment is marked with TODO!
+
+-- Rubric
+-- 
+-- There are three deliverables for this assignment, all delivered via
+-- this file and submitted via GitHub and Canvas:
+-- - A domain model, implemented via CREATE TABLE statements for each
+--   model/table. Also, include DROP TABLE IF EXISTS statements for each
+--   table, so that each run of this script starts with a blank database.
+--   (10 points)
+-- - Insertion of "Batman" sample data into tables (5 points)
+-- - Selection of data, so that something similar to the following sample
+--   "report" can be achieved (5 points)
+
+-- Submission
+-- 
+-- - "Use this template" to create a brand-new "hw1" repository in your
+--   personal GitHub account, e.g. https://github.com/<USERNAME>/hw1
+-- - Do the assignment, committing and syncing often
+-- - When done, commit and sync a final time, before submitting the GitHub
+--   URL for the finished "hw1" repository as the "Website URL" for the 
+--   Homework 1 assignment in Canvas
+
+-- Successful sample output is as shown:
+
+-- Movies
+-- ======
+
+-- Batman Begins          2005           PG-13  Christopher Nolan
+-- The Dark Knight        2008           PG-13  Christopher Nolan
+-- The Dark Knight Rises  2012           PG-13  Christopher Nolan
+
+-- Top Cast
+-- ========
+
+-- Batman Begins          Christian Bale        Bruce Wayne
+-- Batman Begins          Michael Caine         Alfred
+-- Batman Begins          Liam Neeson           Ra's Al Ghul
+-- Batman Begins          Katie Holmes          Rachel Dawes
+-- Batman Begins          Gary Oldman           Commissioner Gordon
+-- The Dark Knight        Christian Bale        Bruce Wayne
+-- The Dark Knight        Heath Ledger          Joker
+-- The Dark Knight        Aaron Eckhart         Harvey Dent
+-- The Dark Knight        Michael Caine         Alfred
+-- The Dark Knight        Maggie Gyllenhaal     Rachel Dawes
+-- The Dark Knight Rises  Christian Bale        Bruce Wayne
+-- The Dark Knight Rises  Gary Oldman           Commissioner Gordon
+-- The Dark Knight Rises  Tom Hardy             Bane
+-- The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
+-- The Dark Knight Rises  Anne Hathaway         Selina Kyle
+
+-- Turns column mode on but headers off
+.mode column
+.headers off
+
+-- Drop existing tables, so you'll start fresh each time this script is run.
+DROP TABLE IF EXISTS My_Movies;
+DROP TABLE IF EXISTS My_Cast;
+
+-- Create new tables, according to your domain model
+CREATE TABLE My_Movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_name TEXT,
+    release_year INTEGER,
+    rating TEXT,
+    director_name TEXT
+);
+
+CREATE TABLE My_Cast (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_id INTEGER,
+    actors_name TEXT,
+    roles_name TEXT
+);
+
+-- Insert data into your database that reflects the sample data shown above
+-- Use hard-coded foreign key IDs when necessary
+INSERT INTO My_Movies (id,movie_name, release_year, rating, director_name)
+VALUES (1,'Batman Begins', 2005,'PG-13','christopher Nolan');
+INSERT INTO My_Movies (id,movie_name, release_year, rating, director_name)
+VALUES (2,'The Dark Knight', 2008,'PG-13','christopher Nolan');
+INSERT INTO My_Movies (id,movie_name, release_year, rating, director_name)
+VALUES (3,'The Dark Knight Rises', 2012,'PG-13','christopher Nolan');
+
+
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (1,1, 'Christian Bale','Bruce Wayne');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (2,1, 'Michael Caine','Alfred Pennyworth');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (3,1, 'Liam Neeson','Ras al Ghul');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (4,1, 'Katie Holmes','Rachel Dawes');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (5,1, 'Gary Oldman','Commissioner Gordon');
+
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (6,2, 'Christian Bale','Bruce Wayne');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (7,2, 'Heath Ledger','Joker');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (8,2, 'Aaron Eckhart','Harvey Dent');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (9,2, 'Michael Caine','Alfred Pennyworth');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (10,2, 'Maggie Gyllenhaal','Rachel Dawes');
+
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (11,3, 'Christian Bale','Bruce Wayne');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (12,3, 'Gary Oldman','Commissioner Gordon');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (13,3, 'Tom Hardy','Bane');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (14,3, 'Joseph Gordon-Levitt','John Blake');
+INSERT INTO My_Cast (id,movie_id, actors_name, roles_name)
+VALUES (15,3,'Anne Hathaway', 'Selina Kyle' );
+
+-- Prints a header for the movies output
+.print "Movies"
+.print "======"
+.print ""
+
+-- The SQL statement for the movies output
+SELECT movie_name, release_year, rating, director_name FROM My_Movies;
+
+-- Prints a header for the cast output
+.print ""
+.print "Top Cast"
+.print "========"
+.print ""
+
+
+-- The SQL statement for the cast output
+SELECT My_Movies.movie_name, My_Cast.actors_name, My_Cast.roles_name FROM My_Cast INNER JOIN My_Movies ON My_Movies.id = My_Cast.movie_id;
